e0979908fe95c0ff26ebb086e7948fd99fcff76f
227Faddi <falukhouma27@gmail.com>
2025-09-26 18:27:38 -0400

    feat: add native config

--- android/src/main/java/expo/modules/alarms/ExpoAlarmsModule.kt
+++ android/src/main/java/expo/modules/alarms/ExpoAlarmsModule.kt
@@ -2,13 +2,16 @@ package expo.modules.alarms
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import android.content.pm.PackageManager
 
-class ExpoAlarmsModule : Module() {
+class ExpoAlarmsModule() : Module() {
   override fun definition() = ModuleDefinition {
     Name("ExpoAlarms")
 
     Function("getApiKey") {
-      return@Function "api-key"
+      val applicationInfo = appContext?.reactContext?.packageManager?.getApplicationInfo(appContext?.reactContext?.packageName.toString(), PackageManager.GET_META_DATA)
+
+      return@Function applicationInfo?.metaData?.getString("MY_CUSTOM_API_KEY")
     }
   }
 }
